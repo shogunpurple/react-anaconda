fc6a9b5a8a498b836bda31ab1174cea6f67fce64
Martin McKeaveney <martinmckeaveney@gmail.com>
2018-06-13 23:58:46 +0100

    basic functionality there, refactoring and tests

--- spec/unit/index.spec.js
+++ spec/unit/index.spec.js
@@ -2,12 +2,76 @@ import React from 'react';
 import Anaconda from 'index';
 import { shallow } from 'enzyme';
 
-describe('React anaconda', () => {
-  it('wraps a component with the passed wrapper component when the condition is true', () => {
-    const wrapper = shallow(<Anaconda 
-        when={true}
-        wrapper={<div something={true} />}
-      />);
-    console.log(wrapper.debug());
+describe('React Anaconda', () => {
+  describe('if the when condition is true and is a boolean', () => {
+    it('wraps a component with the passed wrapper component', () => {
+      const wrapper = shallow(
+        <Anaconda
+          when
+          wrap={children => <div className="wrapper">{children}</div>}>
+          <span>Should wrap </span>
+        </Anaconda>
+      );
+      expect(wrapper.find('.wrapper')).toHaveLength(1);
+      expect(wrapper.find('span')).toHaveLength(1);
+    });
+  });
+
+  describe('if the when condition is a predicate function and the childs props satisfy the predicate', () => {
+    it('wraps a child component with the passed wrapper component', () => {
+      const wrapper = shallow(
+        <Anaconda
+          when={props => props.shouldWrap && props.numPeople > 2}
+          wrap={children => <div className="wrapper">{children}</div>}>
+          <span shouldWrap numPeople={3} />
+        </Anaconda>
+      );
+
+      expect(wrapper.find('.wrapper')).toHaveLength(1);
+    });
+
+    it('wraps a component with the passed wrapper component when children are an array of elements themselves', () => {
+      const wrapper = shallow(
+        <Anaconda
+          wrap={children => <div className="wrapper">{children}</div>}
+          when={props => props.shouldWrap && props.numPeople > 2}>
+          {[1, 2, 3].map(number => (
+            <span key={number} shouldWrap numPeople={number} />
+          ))}
+        </Anaconda>
+      );
+
+      expect(wrapper.find('.wrapper')).toHaveLength(1);
+    });
+  });
+
+  describe('if the when condition is false', () => {
+    it('returns the component as normal', () => {
+      const wrapper = shallow(
+        <Anaconda
+          when={false}
+          wrap={children => <div className="wrapper">{children}</div>}>
+          <div className="unwrapped" />
+        </Anaconda>
+      );
+      expect(wrapper.find('.wrapper')).toHaveLength(0);
+      expect(wrapper.find('.unwrapped')).toHaveLength(1);
+    });
+  });
+
+  describe('when other custom props are passed', () => {
+    it('the rest of the props are passed through to each child component', () => {
+      const wrapper = shallow(
+        <Anaconda name="foo">
+          <div className="unwrapped" />
+          <div className="unwrapped" />
+          <div className="unwrapped" />
+        </Anaconda>
+      );
+
+      wrapper.find('.unwrapped').forEach(node => {
+        expect(node.props().name).toBe('foo');
+      });
+    });
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-export default ({ when, wrapper, children }) => { 
-  return null;
-}
+import React from 'react';
+
+const Anaconda = ({ when, wrap, children, ...rest }) => {
+  const Container = React.Fragment || React.createElement('div');
+
+  // For each child
+  return (
+    <Container>
+      {React.Children.map(children, child => {
+        if (
+          typeof when === 'function' && when(child.props) ||
+          when === true
+        ) {
+          child = wrap(child);
+        }
+        return rest ? React.cloneElement(child, rest) : child;
+      })}
+    </Container>
+  );
+};
+
+export default Anaconda;
